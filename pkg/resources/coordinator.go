dbc9eaca0a090d423bc93439e938f266909b5667
PI-Victor <victor@cloudflavor.io>
2018-04-30 11:57:07 +0200

    coordinator: add basic resource definition

---
+++ pkg/resources/coordinator.go
@@ -0,0 +1,29 @@
+package resources
+
+import (
+	"github.com/PI-Victor/vastup/pkg/up"
+)
+
+const (
+	coordinatorImage = "quay.io/vastness-io/coordinator:latest"
+)
+
+// Coordinator has the functionality to build/slice and operate the vastness
+// resource.
+type Coordinator struct {
+	image    string
+	registry string
+}
+
+// Start will start the container image for coordinator resource
+func (c *Coordinator) Start() error { return nil }
+
+// Build will search for the binary of the compiled coordinator resource within
+// the given context, then it will search the history of the base docker image
+// of the component and get the SHA of the container commit where the binary is
+// added in the base image. It will build a new container image based on that
+// SHA in the history with the new binary copied on top.
+func (c *Coordinator) Build(ctx up.BuildContext) error { return nil }
+
+// pullimage will pull the component's related image from a registry
+func (c *Coordinator) pullImage(ctx up.BuildContext) error { return nil }
