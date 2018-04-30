b7d5caad9d90866701037fab2c1c3f5fcb6e30c0
PI-Victor <victor@cloudflavor.io>
2018-04-30 11:46:07 +0200

    cli: fix var naming

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -5,29 +5,29 @@ import (
 )
 
 var (
-	coordinatorContextPath string
-	vcsWebHookContext      string
+	coordinatorContext string
+	vcsWebHookContext  string
 
 	// Up will start the platform components in the order of dependencies.
 	Up = &cobra.Command{
-		Use:   "",
-		Short: "",
+		Use:   "run",
+		Short: "Bootstrap a new vastness platform instance.=",
 		Run:   func(cmd *cobra.Command, args []string) {},
 	}
 
 	// SetContext will set the component directory context that will be used
 	// to build the component container images.
 	SetContext = &cobra.Command{
-		Use:   "set-context",
-		Short: "",
+		Use:   "context",
+		Short: "Set the build context for a specific component",
 		Run:   func(cmd *cobra.Command, args []string) {},
 	}
 
-	// Setbackend will set the applications backend for storing the bootstrap
+	// SetBackend will set the applications backend for storing the bootstrap
 	// configuration.
 	SetBackend = &cobra.Command{
-		Use:   "set-backend",
-		Short: "",
+		Use:   "backend",
+		Short: "Set the backend for the bootstrap generated configuration",
 		Run:   func(cmd *cobra.Command, args []string) {},
 	}
 )
