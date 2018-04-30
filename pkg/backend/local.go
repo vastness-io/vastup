c58d47ed0f62848262684a36470104999a5dc43e
PI-Victor <victor@cloudflavor.io>
2018-04-30 11:46:07 +0200

    backend: improve local backend storage

--- pkg/backend/local.go
+++ pkg/backend/local.go
@@ -1,18 +1,43 @@
 package backend
 
 import (
+	"path"
+
 	"github.com/PI-Victor/vastup/pkg/up"
 )
 
 // LocalBackend is the default backend used for storing context and component
 // configuration for vastup.
-type LocalBackend struct{}
+type LocalBackend struct {
+	WorkDir string
+}
 
-// Read will return a context
+// Read will return the current context read from the backend.
 func (l *LocalBackend) Read() error {
 	return nil
 }
 
+// Write will store an update to the build context storage.
 func (l *LocalBackend) Write(ctx up.BuildContext) error {
+	return nil
+}
+
+// New will create a new default build context and use Write to store it
+// to disk.
+func (l *LocalBackend) New() error {
+
+	const (
+		repoBinPath = "bin/linux/amd64"
+	)
+
+	newCtx := up.BuildContext{
+		RepoPath: l.WorkDir,
+		BinPath:  path.Join(l.WorkDir, repoBinPath),
+	}
+
+	if err := l.Write(newCtx); err != nil {
+		return err
+	}
+
 	return nil
 }
